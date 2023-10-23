5ff83776c7d12270c8e6a37c265e86b704d0361b
Xiaotian Zhang <xz6917@ic.ac.uk>
2023-10-23 15:59:32 +0100

    commit2

--- api/app.py
+++ api/app.py
@@ -1,8 +1,8 @@
 from flask import Flask, render_template, request
 app = Flask(__name__)
+
+
 @app.route("/submit", methods=["POST"])
-
-
 def submit():
     input_name = request.form.get("name")
     input_age = request.form.get("age")
